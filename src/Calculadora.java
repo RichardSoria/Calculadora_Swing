8b2c09c0afb778b8e8cd8c75880e83b0d5c3f57d
Richard_Soria <richard.soria@epn.edu.ec>
2024-06-23 19:43:03 -0500

    Primer Commit

---
+++ src/Calculadora.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Calculadora {
+    public JPanel mainPanel;
+    private JButton restaButton;
+    private JButton divisiónButton1;
+    private JButton multiplicaciónButton;
+    private JButton sumaButton;
+    private JButton raízCuadradaButton;
+    private JButton senoButton;
+    private JButton potenciaButton;
+    private JButton cosenoButton;
+    private JTextField textField1;
+    private JTextField textField2;
+}
